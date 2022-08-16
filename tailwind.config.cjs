02635073047ca426c006b27b7b0084a38fd89609
Yash patel <yp5516936@gmail.com>
2022-08-15 21:48:39 -0500

    all done

---
+++ tailwind.config.cjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html",],
+  theme: {
+    fontFamily:{
+      "sans":['Poppins',"sans-serif"]
+    },
+    container: {
+      center: true,
+      screens: {
+        "sm": "1520px",
+        
+      },
+    },
+    aspectRatio: {
+      image:'4/3',
+      square:'1/1',
+      vimage:'3/4',
+      cimage: '4/2',
+      dimage: '2/3',
+      eimage: '3/2',
+    },
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
+}
