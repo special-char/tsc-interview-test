c87fa16dad6d3b9feb8758dfbcab5794af2f7062
deepp001 <deepp9185@gmail.com>
2022-08-20 19:32:36 +0530

    completed assignment

---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+  ],
+  theme: {
+    fontFamily:{
+      "sans":['Poppins',"sans-serif"]
+    },
+    container: {
+      center: true,
+      screens: {
+        "sm": '1520px',
+      },
+    },
+    aspectRatio: {
+      "image": '3 / 2',
+      "square": '1 / 1',
+    },
+    
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
+}
